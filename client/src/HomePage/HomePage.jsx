e7e3aee1f2fc4bb1c3bef0d89ebd85f03b20ebc0
gouthamhm <gmanjuna@asu.edu>
2018-09-17 14:56:27 -0700

    feat(login-info): User can view logged in information after logging in
    Signed-off-by: gouthamhm <gmanjuna@asu.edu>

---
+++ client/src/HomePage/HomePage.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+class TimeDispaly extends  React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+            newdate: null
+        };
+    }
+    render(){
+        var newD = new Date(this.value);
+        this.setState({mewdate:newD});
+        return (
+            <ul>{this.state.newdate}</ul>
+        )
+    }
+
+}
+class HomePage extends React.Component {
+    componentDidMount() {
+        this.props.dispatch(userActions.getAll());
+    }
+
+    handleDeleteUser(id) {
+        return (e) => this.props.dispatch(userActions.delete(id));
+    }
+
+    render() {
+        const { user, users } = this.props;
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h1>Hi {user.firstName}!</h1>
+                <p>Welcome! </p>
+                <h3>Your Login History: </h3>
+                {users.loading && <em>Loading users...</em>}
+                {users.error && <span className="text-danger">ERROR: {users.error}</span>}
+                {users.items &&
+                    <ul>
+                        {users.items.map((login, index) =>
+                            <li key={login._id}>
+                                <ul>{login.time}</ul>
+                                {/*<TimeDispaly value = {login.time}/>*/}
+                            </li>
+                        )}
+                    </ul>
+                }
+                <p>
+                    <Link to="/login">Logout</Link>
+                </p>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    const { users, authentication } = state;
+    const { user } = authentication;
+    return {
+        user,
+        users
+    };
+}
+
+const connectedHomePage = connect(mapStateToProps)(HomePage);
+export { connectedHomePage as HomePage };
---
+++ client/src/_services/user.service.js
@@ -0,0 +1,105 @@
+import config from 'config';
+import { authHeader } from '../_helpers';
+
+export const userService = {
+    login,
+    logout,
+    register,
+    getAll,
+    getById,
+    update,
+    delete: _delete
+};
+
+function login(email, password) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+    };
+
+    return fetch(`${config.apiUrl}/auth/login`, requestOptions)
+        .then(handleResponse)
+        .then(user => {
+            debugger;
+            console.log(user);
+            // login successful if there's a jwt token in the response
+            if (user.token) {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('user', JSON.stringify(user));
+            }
+
+            return user;
+        });
+}
+
+function logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('user');
+}
+
+function getAll() {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/logins`, requestOptions).then(handleResponse);
+}
+
+function getById(id) {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse);
+}
+
+function register(user) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    return fetch(`${config.apiUrl}/auth/register`, requestOptions).then(handleResponse);
+}
+
+function update(user) {
+    const requestOptions = {
+        method: 'PUT',
+        headers: { ...authHeader(), 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    return fetch(`${config.apiUrl}/users/${user.id}`, requestOptions).then(handleResponse);;
+}
+
+// prefixed function name with underscore because delete is a reserved word in javascript
+function _delete(id) {
+    const requestOptions = {
+        method: 'DELETE',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse);
+}
+
+function handleResponse(response) {
+    debugger;
+    return response.text().then(text => {
+        debugger;
+        if (text === "Unauthorized") {
+            const data = {error: "UnAuthorized"};
+            logout();
+            return Promise.reject(data.error);
+        }
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            const error = (data && data.error) || response.statusText;
+            return Promise.reject(error);
+        }
+        return data;
+    });
+}
